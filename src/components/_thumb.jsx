20dbe1ee4d32837dbd30e2e91a7cd0713017b4a4
Pectorel <contact@devadra.com>
2023-07-12 04:35:59 +0200

    Added Thumb (_thumb.jsx) component

---
+++ src/components/_thumb.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import styles from "../assets/style/_thumb.module.css";
+
+Thumb.propTypes = {
+  link: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+};
+
+Thumb.propDefault = {
+  link: "",
+};
+
+function Thumb(props) {
+  return (
+    <a href={props.link}>
+      <article
+        className={styles.thumb}
+        style={{ backgroundImage: `url(${props.img})` }}
+      >
+        <h3 className={styles.title}>{props.title}</h3>
+      </article>
+    </a>
+  );
+}
+
+export default Thumb;
