8c5d566b01ff1cab8e3bef3b891c92bad832e7fe
Pectorel <contact@devadra.com>
2023-07-13 20:40:14 +0200

    started location.jsx page + normalized margins behavior

--- src/routes/location.jsx
+++ src/routes/location.jsx
@@ -2,9 +2,13 @@ import Banner from "../components/_banner";
 import { useLoaderData } from "react-router-dom";
 import styles from "../assets/style/location.module.css";
 
+/*
+ * TODO : Change Banner Component for Carousel
+ * */
 function Location() {
   const location = useLoaderData();
-  console.log(location);
+
+  let hostNameSplit = location["host"]["name"].split(/ (.*)/s);
 
   return (
     <>
@@ -15,12 +19,23 @@ function Location() {
             img={location["cover"]}
             className={styles["banner"]}
           ></Banner>
-          <div className={"d-flex"}>
+          <div id={styles["header-details"]} className={"d-flex"}>
             <div>
-              <h1>{location.title}</h1>
-              <h2>{location.location}</h2>
+              <h1 className={styles["title"]}>{location["title"]}</h1>
+              <h2 className={styles["subtitle"]}>{location["location"]}</h2>
             </div>
-            <div></div>
+            <div id={styles["host-details"]} className={"d-flex"}>
+              <p className={styles["host-name"]}>
+                {hostNameSplit[0]}
+                <br />
+                {hostNameSplit[1]}
+              </p>
+              <img
+                className={styles["profile-picture"]}
+                src={location["host"]["picture"]}
+                alt={location["host"]["name"]}
+              />
+            </div>
           </div>
         </header>
       </section>
