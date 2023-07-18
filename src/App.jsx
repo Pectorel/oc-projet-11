caf7bbfe35939d10fc7758b6d5e8851fcb07c964
Pectorel <contact@devadra.com>
2023-07-18 19:18:47 +0200

    Added ScrollRestoration Component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 
 function App() {
   return (
@@ -8,6 +8,7 @@ function App() {
       <Header />
       <Outlet />
       <Footer />
+      <ScrollRestoration />
     </>
   );
 }
